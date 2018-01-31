f96abf6ecb16db6bf310a470d43b9323c3733352
Timothy Hwang <thwang@rocketfuel.com>
2018-01-31 10:44:28 -0500

    Wrapper component

---
+++ src/app/module/wrapper/wrapper.component.ts
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Component, Input, OnInit, OnChanges, AfterViewInit } from '@angular/core';
+import * as uniqueId from 'lodash.uniqueId';
+
+@Component({
+  selector: 'react-wrapper',
+  template: `<div [id]="id"></div>`
+})
+export class WrapperComponent implements OnInit, OnChanges, AfterViewInit {
+  @Input() component: any;
+  @Input() props: any;
+
+  id: string;
+  node: HTMLElement;
+
+  ngOnInit() {
+    this.id = uniqueId('React_');
+  }
+
+  ngAfterViewInit() {
+    this.node = document.getElementById(this.id);
+    this.render();
+  }
+
+  ngOnChanges() {
+    if (this.node) {
+      this.render();
+    }
+  }
+
+  render() {
+    ReactDOM.render(React.createElement(this.component, this.props), this.node);
+  }
+}
---
+++ src/app/module/wrapper/wrapper.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { WrapperComponent } from './wrapper.component';
+
+@NgModule({
+  declarations: [WrapperComponent],
+  exports: [WrapperComponent]
+})
+export class WrapperModule {}
