4d6ea6f3c51cb17558d3a7b2409ca97f58c5ee8d
Timothy Hwang <thwang@rocketfuel.com>
2018-01-31 10:48:40 -0500

    Initial prettier run

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,8 @@ import DemoReactComponent from './DemoReactComponent';
       <react-wrapper [component]="component3"></react-wrapper>
       <button (click)="handleClick()">I am an Angular button</button>
     </div>
-  `})
+  `
+})
 export class AppComponent {
   component1 = 'span';
   props1: any = {
@@ -31,5 +32,5 @@ export class AppComponent {
   handleClick = () => {
     this.props1 = { children: this.props1.children + ' Angular was here!' };
     this.props2 = { ...this.props2, children: this.props2.children + ' Angular was also here!' };
-  }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { WrapperModule } from './module/wrapper/wrapper.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    WrapperModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WrapperModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
